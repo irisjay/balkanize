c90073e101498b3f16690784004cceaea227bb8f
irisjay <irisjay@connect.hku.hk>
2018-08-10 17:34:28 +0800

    *

---
+++ balkans.js
@@ -0,0 +1,27 @@
+var Z = require ('sanctuary')
+/*
+var $ = require ('sanctuary-def')
+
+
+var and = 
+
+var or = (...kinds) =>
+	$ .NullaryType ('DisjointUnion', 'ask jay', x => kinds .any (y => y ._test (x)))
+*/
+
+/*
+var and = (...kinds) => 
+	Z .fromPairs (kinds .map ())
+	(...x) =>
+
+var or = (...kinds) => {
+	var constructors = kinds .map (x =>
+		!! (x .constructor === $ .Type) ?
+			
+		: !! (x .constructor === RegExp))
+		:
+	var }
+
+*/
+
+var data = constructors => Z .map (x => (...args) => args, constructors)
